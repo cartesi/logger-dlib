1afeed9ee689f784285773f6f7ede5135004d6a6
Stephen <stephen@cartesi.io>
2019-12-10 23:52:24 +0800

    Add more check on blockchain to avoid duplicate submission

--- logger/logger.py
+++ logger/logger.py
@@ -11,6 +11,7 @@
 
 import sys
 import os
+import sha3
 
 
 class Logger:
@@ -22,6 +23,25 @@ class Logger:
         self.__logger = self.__w3.eth.contract(address=logger_address, abi=logger_abi)
         self.__bytes_of_word = 8
         self.__debug = False
+
+    def instantiate(self, page_log2_size, tree_log2_size):
+        self.__page_log_2_size = page_log2_size
+        self.__tree_log_2_size = tree_log2_size
+        self.__page_size = 2**self.__page_log_2_size
+        self.__tree_size = 2**self.__tree_log_2_size
+        self.__download_cache = {}
+        # submission cache from on-chain logger
+        # map {hash} value to {index} in logger history
+        self.__submission_root_cache = {}
+        # local cache
+        # map {bytes} blob to {index}
+        self.__submission_blob_cache = {}
+        # map {indices} tuple to {index}
+        self.__submission_index_cache = {}
+
+        if (not self.__w3.isConnected()):
+            print("Couldn't connect to node, exiting")
+            sys.exit(1)
 
     def __bytes_from_file(self, filename):
         with open(filename, "rb") as f:
@@ -65,22 +85,47 @@ class Logger:
         except ValueError as e:
             print(str(e))
 
-    def instantiate(self, page_log2_size, tree_log2_size):
-        self.__page_log_2_size = page_log2_size
-        self.__tree_log_2_size = tree_log2_size
-        self.__page_size = 2**self.__page_log_2_size
-        self.__tree_size = 2**self.__tree_log_2_size
-        self.__download_cache = {}
-        self.__submission_blob_cache = {}
-        self.__submission_index_cache = {}
+    def __calculate_root_from_hashes(self, hashes):
+        
+        while len(hashes) > 1:
+            results = []
+            for x in range(int(len(hashes) / 2)):
+                left = hashes.pop(0)
+                right = hashes.pop(0)
 
-        if (not self.__w3.isConnected()):
-            print("Couldn't connect to node, exiting")
-            sys.exit(1)
+                k = sha3.keccak_256()
+                k.update(left + right)
+                results.append(bytes.fromhex(k.hexdigest()))
+            hashes = results
+
+        return hashes[0]
+
+    def __get_index_from_root(self, root):
+
+        # check if submission exists in the history
+        if self.__logger.functions.isLogAvailable(root).call():
+            index = self.__logger.functions.getLogIndex(root).call()
+            if(self.__debug):
+                print("find root %s in history %d" % (root.hex(), index))
+            return (True, index)
+        
+        return (False, 0)
 
     def submit_indices_to_logger(self, log2_size, indices):
 
         try:
+            # get hashes from history with index
+            hashes = []
+            for x in indices:
+                hashes.append(self.__logger.functions.getLogRoot(x).call())
+
+            root = self.__calculate_root_from_hashes(hashes)
+
+            (exists, index) = self.__get_index_from_root(root)
+            if exists:
+                return (index, root)
+
+            # submit data if is the first time
             nonce = self.__w3.eth.getTransactionCount(self.__user)
             txn = self.__logger.functions.calculateMerkleRootFromHistory(log2_size, indices).buildTransaction({"nonce": nonce, "from": self.__user})
             signed_txn = self.__w3.eth.account.sign_transaction(txn, private_key=self.__key)
@@ -107,6 +152,23 @@ class Logger:
     def submit_data_to_logger(self, data):
 
         try:
+            # calculate hash locally
+            hashes = []
+            padded_data = list(data)
+            for x in range(self.__page_size - len(data)):
+                padded_data.append(bytes(self.__bytes_of_word))
+            for x in range(self.__page_size):
+                k = sha3.keccak_256()
+                k.update(padded_data[x])
+                hashes.append(bytes.fromhex(k.hexdigest()))
+
+            root = self.__calculate_root_from_hashes(hashes)
+
+            (exists, index) = self.__get_index_from_root(root)
+            if exists:
+                return (index, root)
+
+            # submit data if is the first time
             nonce = self.__w3.eth.getTransactionCount(self.__user)
             txn = self.__logger.functions.calculateMerkleRootFromData(self.__page_log_2_size, data).buildTransaction({"nonce": nonce, "from": self.__user})
             signed_txn = self.__w3.eth.account.sign_transaction(txn, private_key=self.__key)
@@ -155,8 +217,7 @@ class Logger:
             count -= 1
 
         data = []
-        for x in range(self.__page_size):
-            data.append(bytes(self.__bytes_of_word))
+        data.append(bytes(self.__bytes_of_word))
 
         if(count > 0):
             (index, root) = self.submit_data_to_logger(data)
@@ -166,9 +227,8 @@ class Logger:
 
         index_log2_size = self.__page_log_2_size
         while(len(indices) > 1):
-            indices_len = len(indices)
             new_indices = []
-            for x in range(int(indices_len / 2)):
+            for x in range(int(len(indices) / 2)):
                 partial_indices = []
                 partial_indices.append(indices.pop(0))
                 partial_indices.append(indices.pop(0))
